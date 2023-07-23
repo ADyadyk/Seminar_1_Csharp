02b2998936c79bc491d8b5245dd03cc77e0000b3
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-07-23 22:17:49 +0300

    add Task 008 ROW OF EVEN NUMBERS

---
+++ Task008_row_of_even_numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+
+Console.Write("Введите число N=");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Чётные числа от 1 до {n}: ");
+
+int count = 2;
+while (count <= n)
+{
+    int remainder = count % 2;
+    if (remainder == 0) Console.Write($"{count}  ");
+    count++;
+}
