5460faafa0d5677cda6a6d8bc5a05acf688b2f7c
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-07-23 21:27:01 +0300

    add Task002 MAX MIN NUMBERS

---
+++ Task002_max_min/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая на входе принимает два числа и выдает,
+какое число больше, а какое меньше.
+a = 5; b = 7 -> max = 7
+a = 2; b = 10 -> max = 10
+a = -9; b = -3 -> max = -3
+*/
+
+Console.Write("Введите число: a=");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число: b=");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b) Console.WriteLine($"Число a={a} больше числа b={b}");
+if (b > a) Console.WriteLine($"Число b={b} больше числа a={a}");
+if (a == b) Console.WriteLine($"Число a={a} равно числу b={b}");
