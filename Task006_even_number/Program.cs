afa2c23bb815099ee63cca55634625d44e3b72d9
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-07-23 22:00:58 +0300

    add Task 006 EVEN NUMBER

---
+++ Task006_even_number/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+4 -> yes
+-3 -> no
+7 -> no
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int remainder = number % 2;
+
+if (remainder == 0) Console.WriteLine("Введённое число является чётным.");
+else Console.WriteLine("Введённое число является не чётным.");
