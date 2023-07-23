0f0696d7c264a7b0c93bb473086da16f9908aae2
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-07-23 18:05:59 +0300

    add Example 004 NUMBER GAP

---
+++ Example004_number_gap/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа выводит числа от -n до n
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = -number;
+
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
